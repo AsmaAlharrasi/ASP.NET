79a860a4f37f673c8d451c5011b19f71065040ab
AsmaAlharrasi <asmaalharrasi18@gmail.com>
2024-01-14 14:47:24 +0400

    clinic

--- MVC/BulkyWeb/Bulky solution/Bulky/Program.cs
+++ MVC/BulkyWeb/Bulky solution/Bulky/Program.cs
@@ -33,7 +33,7 @@ namespace Bulky
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Category}/{action=Index}/{id?}");
 
             app.Run();
         }
